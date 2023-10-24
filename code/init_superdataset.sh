ebe7af72051a65d832521a0f6f5e1b181b37d8ad
Taylor Salo <tsalo006@fiu.edu>
2023-10-24 10:37:01 -0400

    Update init_superdataset.sh

--- code/init_superdataset.sh
+++ code/init_superdataset.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
 # Install an OpenNeuro dataset.
+#
+# Must define DATALAD_CREDENTIAL_GIN_TOKEN environment variable.
 dataset_id=$1
-base_dir="/cbica/home/salot/datasets"
+base_dir="/cbica/home/salot/open-multi-echo-data/datasets"
 superdataset_dir=${base_dir}/${dataset_id}
-code_dir=`pwd`
+code_dir="/cbica/home/salot/open-multi-echo-data/code"
 
 # Create the YODA superdataset
 datalad create -c yoda \
@@ -28,11 +30,24 @@ datalad create -d ${superdataset_dir} \
 
 # Prepare code directory
 mkdir ${superdataset_dir}/code
-cp ${code_dir}/run_afni_proc.sh ${superdataset_dir}/code/
 cp ${code_dir}/run_fmriprep.sh ${superdataset_dir}/code/
+cp ${code_dir}/run_afni_proc.sh ${superdataset_dir}/code/
 datalad save -m "Add base scripts for preprocessing."
 
+# Push raw subdataset to G-Node GIN
+datalad create-sibling-gin \
+    --siblingname gin \
+    --access-protocol ssh \
+    --dataset inputs/data \
+    --credential GIN \
+    ME-ICA/${dataset_id}_raw
+datalad push -d inputs/data --to gin
+
 # Push superdataset to G-Node GIN
-datalad create-sibling-gin --siblingname gin --access-protocol ssh \
-    --dataset . ME-ICA/${dataset_id}_superdataset
-datalad push --to gin
+datalad create-sibling-gin \
+    --siblingname gin \
+    --access-protocol ssh \
+    --dataset . \
+    --credential GIN \
+    ME-ICA/${dataset_id}_superdataset
+datalad push -d . --to gin
