3ebff67eadb1b9245c494f9628583cff4256ea4f
Taylor Salo <tsalo006@fiu.edu>
2023-10-24 10:40:28 -0400

    Update.

--- code/init_superdataset.sh
+++ code/init_superdataset.sh
@@ -6,7 +6,8 @@
 dataset_id=$1
 base_dir="/cbica/home/salot/open-multi-echo-data/datasets"
 superdataset_dir=${base_dir}/${dataset_id}
-code_dir="/cbica/home/salot/open-multi-echo-data/code"
+raw_dataset_dir=${superdataset_dir}/inputs/data
+code_dir="/cbica/home/salot/open-multi-echo-data/code/code"
 
 # Create the YODA superdataset
 datalad create -c yoda \
@@ -17,37 +18,37 @@ cd ${superdataset_dir}
 # Download the OpenNeuro subdataset
 datalad clone -d ${superdataset_dir} \
     -D "Clone of OpenNeuro dataset. May be modified for fMRIPrep/AFNI and pushed to G-Node GIN." \
-    https://github.com/OpenNeuroDatasets/${dataset_id}.git inputs/data
-datalad get inputs/data
+    https://github.com/OpenNeuroDatasets/${dataset_id}.git ${raw_dataset_dir}
+datalad get ${raw_dataset_dir}
 
 # Create output subdatasets
 datalad create -d ${superdataset_dir} \
     -D "fMRIPrep derivatives for ${dataset_id}." \
-    outputs/fmriprep
+    ${superdataset_dir}/outputs/fmriprep
 datalad create -d ${superdataset_dir} \
     -D "AFNI derivatives for ${dataset_id}." \
-    outputs/afni
+    ${superdataset_dir}/outputs/afni
 
 # Prepare code directory
 mkdir ${superdataset_dir}/code
 cp ${code_dir}/run_fmriprep.sh ${superdataset_dir}/code/
 cp ${code_dir}/run_afni_proc.sh ${superdataset_dir}/code/
-datalad save -m "Add base scripts for preprocessing."
+datalad save -d ${superdataset_dir} -m "Add base scripts for preprocessing."
 
 # Push raw subdataset to G-Node GIN
 datalad create-sibling-gin \
     --siblingname gin \
     --access-protocol ssh \
-    --dataset inputs/data \
+    --dataset ${raw_dataset_dir} \
     --credential GIN \
     ME-ICA/${dataset_id}_raw
-datalad push -d inputs/data --to gin
+datalad push -d ${raw_dataset_dir} --to gin
 
 # Push superdataset to G-Node GIN
 datalad create-sibling-gin \
     --siblingname gin \
     --access-protocol ssh \
-    --dataset . \
+    --dataset ${superdataset_dir} \
     --credential GIN \
     ME-ICA/${dataset_id}_superdataset
-datalad push -d . --to gin
+datalad push -d ${superdataset_dir} --to gin
