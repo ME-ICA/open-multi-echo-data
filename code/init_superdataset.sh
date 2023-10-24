c0ae0a1957d9fdc9eb6490744d8b6f434e46c233
Taylor Salo <tsalo006@fiu.edu>
2023-10-24 07:34:17 -0400

    Update init_superdataset.sh

--- code/init_superdataset.sh
+++ code/init_superdataset.sh
@@ -2,17 +2,37 @@
 #
 # Install an OpenNeuro dataset.
 dataset_id=$1
-datalad create -c yoda -D "Create superdataset for OpenNeuro dataset ${dataset_id}" "${dataset_id}"`
-cd ${dataset_id}
-datalad clone -d . -D "Clone of OpenNeuro dataset. May be modified to work with fMRIPrep/AFNI and pushed to G-Node GIN." https://github.com/OpenNeuroDatasets/${dataset_id}.git inputs/data
-cd inputs/data
-datalad get .
-cd ../..
-datalad create -d . -D "fMRIPrep derivatives for ${dataset_id}." outputs/fmriprep
-datalad create -d . -D "AFNI derivatives for ${dataset_id}." outputs/afni
-mkdir code
-cp run_afni_proc.sh code/
-cp run_fmriprep.sh code/
+base_dir="/cbica/home/salot/datasets"
+superdataset_dir=${base_dir}/${dataset_id}
+code_dir=`pwd`
+
+# Create the YODA superdataset
+datalad create -c yoda \
+    -D "Create superdataset for OpenNeuro dataset ${dataset_id}" \
+    "${superdataset_dir}"`
+cd ${superdataset_dir}
+
+# Download the OpenNeuro subdataset
+datalad clone -d ${superdataset_dir} \
+    -D "Clone of OpenNeuro dataset. May be modified for fMRIPrep/AFNI and pushed to G-Node GIN." \
+    https://github.com/OpenNeuroDatasets/${dataset_id}.git inputs/data
+datalad get inputs/data
+
+# Create output subdatasets
+datalad create -d ${superdataset_dir} \
+    -D "fMRIPrep derivatives for ${dataset_id}." \
+    outputs/fmriprep
+datalad create -d ${superdataset_dir} \
+    -D "AFNI derivatives for ${dataset_id}." \
+    outputs/afni
+
+# Prepare code directory
+mkdir ${superdataset_dir}/code
+cp ${code_dir}/run_afni_proc.sh ${superdataset_dir}/code/
+cp ${code_dir}/run_fmriprep.sh ${superdataset_dir}/code/
 datalad save -m "Add base scripts for preprocessing."
-datalad create-sibling-gin --siblingname gin --access-protocol ssh --dataset . ME-ICA/${dataset_id}_superdataset
+
+# Push superdataset to G-Node GIN
+datalad create-sibling-gin --siblingname gin --access-protocol ssh \
+    --dataset . ME-ICA/${dataset_id}_superdataset
 datalad push --to gin
