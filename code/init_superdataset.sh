26883bccc2f345bb5341bd0e659f70057c97233a
tsalo <tsalo90@gmail.com>
2023-10-25 12:13:10 -0400

    Update scripts.

--- code/init_superdataset.sh
+++ code/init_superdataset.sh
@@ -12,19 +12,21 @@ code_dir="/cbica/home/salot/open-multi-echo-data/code/code"
 # Create the YODA superdataset
 datalad create -c yoda \
     -D "Create superdataset for OpenNeuro dataset ${dataset_id}" \
-    "${superdataset_dir}"`
+    "${superdataset_dir}"
 cd ${superdataset_dir}
 
 # Download the OpenNeuro subdataset
 datalad clone -d ${superdataset_dir} \
     -D "Clone of OpenNeuro dataset. May be modified for fMRIPrep/AFNI and pushed to G-Node GIN." \
-    https://github.com/OpenNeuroDatasets/${dataset_id}.git ${raw_dataset_dir}
+    https://github.com/ME-ICA/${dataset_id}.git ${raw_dataset_dir}
+
 datalad get ${raw_dataset_dir}
 
 # Create output subdatasets
 datalad create -d ${superdataset_dir} \
     -D "fMRIPrep derivatives for ${dataset_id}." \
     ${superdataset_dir}/outputs/fmriprep
+
 datalad create -d ${superdataset_dir} \
     -D "AFNI derivatives for ${dataset_id}." \
     ${superdataset_dir}/outputs/afni
@@ -37,18 +39,18 @@ datalad save -d ${superdataset_dir} -m "Add base scripts for preprocessing."
 
 # Push raw subdataset to G-Node GIN
 datalad create-sibling-gin \
-    --siblingname gin \
     --access-protocol ssh \
     --dataset ${raw_dataset_dir} \
     --credential GIN \
     ME-ICA/${dataset_id}_raw
+
 datalad push -d ${raw_dataset_dir} --to gin
 
 # Push superdataset to G-Node GIN
 datalad create-sibling-gin \
-    --siblingname gin \
     --access-protocol ssh \
     --dataset ${superdataset_dir} \
     --credential GIN \
     ME-ICA/${dataset_id}_superdataset
+
 datalad push -d ${superdataset_dir} --to gin
---
+++ code/run_init.sh
@@ -0,0 +1,1 @@
+source /cbica/home/salot/open-multi-echo-data/code/code/init_superdataset.sh ds002156
