181546872eedf63be620ecd6a3c14c3fea995fe8
tsalo <tsalo90@gmail.com>
2024-03-09 09:45:57 -0500

    Update scripts a bit.

--- code/init_superdataset.sh
+++ code/init_superdataset.sh
@@ -3,7 +3,7 @@
 # Install an OpenNeuro dataset.
 dataset_id=$1
 base_dir="/cbica/home/salot/open-multi-echo-data/datasets"
-superdataset_dir=${base_dir}/${dataset_id}_test
+superdataset_dir=${base_dir}/${dataset_id}
 raw_dataset_dir=${superdataset_dir}/inputs/data
 code_dir="/cbica/home/salot/open-multi-echo-data/code/code"
 
@@ -15,7 +15,7 @@ cd ${superdataset_dir}
 
 # Download the OpenNeuro subdataset
 datalad clone -d ${superdataset_dir} \
-    -D "Clone of OpenNeuro dataset. May be modified for fMRIPrep/AFNI and pushed to G-Node GIN." \
+    -D "Clone of OpenNeuro dataset. May be modified for fMRIPrep/AFNI and pushed to OpenNeuro." \
     https://github.com/ME-ICA/${dataset_id}.git ${raw_dataset_dir}
 
 datalad get ${raw_dataset_dir}
--- code/pull_singularity_images.sh
+++ code/pull_singularity_images.sh
@@ -1,2 +1,2 @@
 singularity pull --name /cbica/home/salot/open-multi-echo-data/singularity/afni-23_1_10.simg docker://afni/afni_make_build:AFNI_23.1.10
-singularity pull --name /cbica/home/salot/open-multi-echo-data/singularity/fmriprep-23_1_5dev.simg docker://nipreps/fmriprep:unstable
+# singularity pull --name /cbica/home/salot/open-multi-echo-data/singularity/fmriprep-23_2_1.simg docker://nipreps/fmriprep:23.2.1
