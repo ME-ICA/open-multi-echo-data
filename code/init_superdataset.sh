ebc895b32f3098c052c1156e4c9e7ef70df755ce
tsalo <tsalo90@gmail.com>
2024-05-03 16:14:46 -0400

    Update scripts.

--- code/init_superdataset.sh
+++ code/init_superdataset.sh
@@ -13,12 +13,23 @@ datalad create -c yoda \
     "${superdataset_dir}"
 cd ${superdataset_dir}
 
+# Fork the OpenNeuro dataset to ME-ICA if it doesn't exist
+
+# Check if the repository exists
+if gh repo view ME-ICA/$dataset_id &>/dev/null; then
+    echo "ME-ICA/$dataset_id exists"
+else
+    # gh repo fork OpenNeuroDatasets/${dataset_id} --org "ME-ICA"
+    echo "Repository does not exist"
+fi
+
 # Download the OpenNeuro subdataset
 datalad clone -d ${superdataset_dir} \
     -D "Clone of OpenNeuro dataset. May be modified for fMRIPrep/AFNI and pushed to OpenNeuro." \
     https://github.com/ME-ICA/${dataset_id}.git ${raw_dataset_dir}
 
-datalad get ${raw_dataset_dir}
+# Don't download the data
+# datalad get ${raw_dataset_dir}
 mkdir -p ${raw_dataset_dir}/code
 
 # Create output subdatasets
@@ -34,3 +45,4 @@ datalad create -d ${superdataset_dir} \
 cp ${code_dir}/run_fmriprep.sh ${superdataset_dir}/code/
 cp ${code_dir}/run_afni_proc.sh ${superdataset_dir}/code/
 datalad save -d ${superdataset_dir} -m "Add base scripts for preprocessing."
+cd ${code_dir}
