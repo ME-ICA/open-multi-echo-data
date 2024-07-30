23491c0b743f01dcfad8d3fef0a07198e089d111
tsalo <tsalo90@gmail.com>
2024-07-30 13:08:16 -0400

    Update.

--- code/pull_singularity_images.sh
+++ code/pull_singularity_images.sh
@@ -1,2 +1,2 @@
-# singularity pull --name /cbica/home/salot/open-multi-echo-data/singularity/afni-23_1_10.sif docker://afni/afni_make_build:AFNI_23.1.10
-singularity pull --name /cbica/home/salot/open-multi-echo-data/singularity/fmriprep-23_2_2.sif docker://nipreps/fmriprep:23.2.2
+apptainer pull --name /cbica/home/salot/open-multi-echo-data/singularity/afni-23_1_10.sif docker://afni/afni_make_build:AFNI_23.1.10
+# apptainer pull --name /cbica/home/salot/open-multi-echo-data/singularity/fmriprep-23_2_2.sif docker://nipreps/fmriprep:23.2.2
